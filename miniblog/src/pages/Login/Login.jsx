e65b23e28cc5f2589939cd08a2c78d3cea566a8b
Luiz Felipe Zomer <129988357+LuizZomer@users.noreply.github.com>
2023-11-15 20:58:47 -0300

    correção de erro

--- miniblog/src/pages/Login/Login.jsx
+++ miniblog/src/pages/Login/Login.jsx
@@ -63,7 +63,7 @@ const Login = () => {
              onChange={(e) => setPassword(e.target.value)}
             />
           </label>
-          {!loading && <button className='btn'>Cadastrar</button>}
+          {!loading && <button className='btn'>Entrar</button>}
           {loading && <button className='btn' disabled>
             Aguarde...
           </button> }
